7e1ff3d7b8faa69a0e9ae97e17581ffb8f65b8c8
TallesMP <tallespereira1@gmail.com>
2024-12-31 23:47:49 -0300

    testing db connection

---
+++ backend/src/db/connection.ts
@@ -0,0 +1,15 @@
+import { Client } from 'pg'
+
+
+const client = new Client({
+  user: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  database: 'controle_gastos'
+});
+
+
+client.connect()
+  .then(() => console.log("Conectado"))
+  .catch(err => console.log("erro", err));
+
