b83c0c508d158c5c9624fbfabf1dd1245a8ac57e
Thaylan <taylan-150963@hotmail.com>
2025-03-08 18:33:22 -0300

    Added edit category, get category, and get categories

--- backend/src/controller/CategoryController.ts
+++ backend/src/controller/CategoryController.ts
@@ -1,28 +1,74 @@
 import { CategoryService } from "../services/CategoryService";
 import { Request, Response } from "express";
 export class CategoryController {
-  static async createCategory(req: Request, res: Response) {
+  static async getCategories(req: Request, res: Response) {
     try {
-      const { name } = req.body;
-      await CategoryService.createCategory(res.locals.UserToken.id, name)
-      res.status(201).json({ message: "Categoria criada" })
+      const categories = await CategoryService.getCategories(
+        res.locals.UserToken.id
+      );
+
+      res.status(201).json({ message: "Categorias retornadas", categories });
     } catch (error: any) {
       res
+
         .status(error.status || 500)
-        .json({ error: error.message || "Erro inesperado" })
-    }
-  }
-  static async removeCategory(req: Request, res: Response) {
-    try {
-      const { name } = req.body;
-      await CategoryService.removeCategory(res.locals.UserToken.id, name)
-      res.status(201).json({ message: "Categoria excluida" })
-    }
-    catch (error: any) {
-      res
-        .status(error.status || 500)
-        .json({ error: error.message || "Erro inesperado" })
+        .json({ error: error.message || "Erro inesperado" });
     }
   }
 
+  static async getCategory(req: Request, res: Response) {
+    try {
+      const { category_id } = req.body;
+      const categories = await CategoryService.getCategory(category_id);
+
+      res.status(201).json({ message: "Categoria retornada", categories });
+    } catch (error: any) {
+      res
+
+        .status(error.status || 500)
+        .json({ error: error.message || "Erro inesperado" });
+    }
+  }
+
+  static async createCategory(req: Request, res: Response) {
+    try {
+      const { name } = req.body;
+
+      await CategoryService.createCategory(res.locals.UserToken.id, name);
+      res.status(201).json({ message: "Categoria criada" });
+    } catch (error: any) {
+      res
+
+        .status(error.status || 500)
+        .json({ error: error.message || "Erro inesperado" });
+    }
+  }
+
+  static async removeCategory(req: Request, res: Response) {
+    try {
+      const { name } = req.body;
+      await CategoryService.removeCategory(res.locals.UserToken.id, name);
+      res.status(201).json({ message: "Categoria excluida" });
+    } catch (error: any) {
+      res
+        .status(error.status || 500)
+        .json({ error: error.message || "Erro inesperado" });
+    }
+  }
+
+  static async editCategory(req: Request, res: Response) {
+    try {
+      const { name, newName } = req.body;
+      await CategoryService.editCategory(
+        res.locals.UserToken.id,
+        name,
+        newName
+      );
+      res.status(201).json({ message: "Categoria editada" });
+    } catch (error: any) {
+      res
+        .status(error.static || 500)
+        .json({ error: error.message || "Erro inesperado" });
+    }
+  }
 }
--- backend/src/repositories/CategoryRepository.ts
+++ backend/src/repositories/CategoryRepository.ts
@@ -1,50 +1,78 @@
 import client from "../db/connection";
 export class CategoryRepository {
-  static async getCategory(user_id: number) {
+  static async getCategories(user_id: number) {
     const query = `
-    SELECT * FROM categories
-    WHERE user_id = $1`
+    SELECT category_id, name  FROM categories
+    WHERE user_id = $1`;
     try {
-      return await client.query(query, [user_id])
+      return (await client.query(query, [user_id])).rows;
     } catch (error: any) {
-      throw { status: 409, message: "Não foi possível buscar categoria" }
+      throw { status: 409, message: "Não foi possível buscar categoria" };
     }
   }
+
+  static async getCategory(category_id: number) {
+    const query = `
+    SELECT * FROM categories
+    WHERE category_id = $1`;
+    try {
+      return (await client.query(query, [category_id])).rows[0];
+    } catch (error: any) {
+      throw { status: 409, message: "Não foi possível buscar categoria" };
+    }
+  }
+
   static async findCategory(user_id: number, name: string) {
     const query = `
     SELECT * FROM categories
-    WHERE user_id = $1 AND name = $2`
+    WHERE user_id = $1 AND name = $2`;
     try {
-      const result = await client.query(query, [user_id, name])
-      return result.rowCount != 0
+      const result = await client.query(query, [user_id, name]);
+      return result.rowCount != 0;
     } catch (error: any) {
-      console.log(error.message)
+      console.log(error.message);
       throw { status: 409, message: "Não foi possivel buscar categoria" };
     }
   }
   static async insertCategory(user_id: number, name: string) {
     const query = `
     INSERT INTO categories (user_id, name)
-    VALUES ($1, $2);`
+    VALUES ($1, $2);`;
     try {
-      await client.query(query, [user_id, name])
+      await client.query(query, [user_id, name]);
     } catch (error: any) {
-      console.log("Erro no Repository: " + error.message)
+      console.log("Erro no Repository: " + error.message);
       throw { status: 409, message: "Não foi possivel criar categoria" };
     }
   }
   static async deleteCategory(user_id: number, name: string) {
     const query = `
     DELETE FROM categories 
-    WHERE user_id = $1 AND name = $2`
+    WHERE user_id = $1 AND name = $2`;
     try {
-      await client.query(query, [user_id, name])
-      console.log("Categoria " + name + " apagada")
+      await client.query(query, [user_id, name]);
+      console.log("Categoria " + name + " apagada");
     } catch (error: any) {
-      console.log("Erro no Repository: " + error.message)
+      console.log("Erro no Repository: " + error.message);
       throw { status: 409, message: "Não foi possivel excluir categoria" };
     }
   }
+
+  static async editCategory(user_id: number, name: string, newName: string) {
+    const query = `
+      UPDATE categories 
+      SET name = $1 
+      WHERE name = $2 AND user_id = $3  
+    `;
+
+    try {
+      console.log(newName, name, user_id);
+
+      await client.query(query, [newName, name, user_id]);
+      console.log("Categoria " + name + "editada");
+    } catch (error: any) {
+      console.log("Erro no Repository: " + error.message);
+      throw { status: 409, message: "Não foi possivel editar  categoria" };
+    }
+  }
 }
-
-
